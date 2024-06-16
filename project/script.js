36af58fed6838aef093ce1b0b8dd98cc582906aa
lsy4982sm <lsy4982@sookmyung.ac.kr>
2024-06-16 17:59:56 +0900

    commit

---
+++ project/script.js
@@ -0,0 +1,29 @@
+/*menu1 : 나에게 맞는 한국 드라마 찾기*/
+document.getElementById('m1').addEventListener('click', function() {
+    window.location.href = 'menu1.html';
+  });
+
+  /*menu2 : 전체 추천 목록*/
+document.getElementById('m2').addEventListener('click', function() {
+    window.location.href = 'menu2.html';
+  });
+
+  /*menu3 : 나의 인생 드라마*/
+document.getElementById('m3').addEventListener('click', function() {
+    window.location.href = 'menu3.html';
+  });
+
+  /*menu4 : 2024 하반기 방영 예정작*/
+document.getElementById('m4').addEventListener('click', function() {
+    window.location.href = 'menu4.html';
+  });
+
+  /*menu5 : 나의 위시리스트*/
+document.getElementById('m5').addEventListener('click', function() {
+    window.location.href = 'menu5.html';
+  });
+
+/*menu6 : 제보하기*/
+document.getElementById('m6').addEventListener('click', function() {
+    window.location.href = 'menu6.html';
+  });
