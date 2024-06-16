781156d9dcb6adbcffba0f969f2a86f88c214a03
lsy4982sm <lsy4982@sookmyung.ac.kr>
2024-06-16 23:33:04 +0900

    커밋

--- project/script.js
+++ project/script.js
@@ -8,6 +8,39 @@ document.getElementById('m2').addEventListener('click', function() {
     window.location.href = 'menu2.html';
   });
 
+  /*menu2 전체 추천 목록에서 이미지 누르면 드라마 소개로 이동*/
+  document.getElementById('img1').addEventListener('click', function() {
+    window.location.href = 'drama1.html';
+  });
+  document.getElementById('img2').addEventListener('click', function() {
+    window.location.href = 'drama2.html';
+  });
+  document.getElementById('img3').addEventListener('click', function() {
+    window.location.href = 'drama3.html';
+  });
+  document.getElementById('img4').addEventListener('click', function() {
+    window.location.href = 'drama4.html';
+  });
+  document.getElementById('img5').addEventListener('click', function() {
+    window.location.href = 'drama5.html';
+  });
+  document.getElementById('img6').addEventListener('click', function() {
+    window.location.href = 'drama6.html';
+  });
+  document.getElementById('img7').addEventListener('click', function() {
+    window.location.href = 'drama7.html';
+  });
+  document.getElementById('img8').addEventListener('click', function() {
+    window.location.href = 'drama8.html';
+  });
+  document.getElementById('img9').addEventListener('click', function() {
+    window.location.href = 'drama9.html';
+  });
+  document.getElementById('img10').addEventListener('click', function() {
+    window.location.href = 'drama10.html';
+  });
+
+
   /*menu3 : 나의 인생 드라마*/
 document.getElementById('m3').addEventListener('click', function() {
     window.location.href = 'menu3.html';
