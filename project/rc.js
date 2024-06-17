0e5d4d82fef1e65b807955450b9593180f86bb4e
lsy4982sm <lsy4982@sookmyung.ac.kr>
2024-06-18 00:36:13 +0900

    commit

---
+++ project/rc.js
@@ -0,0 +1,61 @@
+
+document.getElementById('b1').addEventListener('click', function() {
+    window.location.href = 'menu1-1.html';
+  });
+
+
+document.getElementById('b1-1').addEventListener('click', function() {
+    window.location.href = 'menu1-1-1.html';
+  });
+
+document.getElementById('b1-2').addEventListener('click', function() {
+    window.location.href = 'menu1-1-2.html';
+  });
+
+document.getElementById('b2').addEventListener('click', function() {
+    window.location.href = 'menu1-2.html';
+  });
+
+
+
+/*최종 결과 확인*/
+
+document.getElementById('r1').addEventListener('click', function() {
+    window.location.href = 'drama1.html';
+  });
+
+document.getElementById('r2').addEventListener('click', function() {
+    window.location.href = 'drama2.html';
+  });
+
+document.getElementById('r3').addEventListener('click', function() {
+    window.location.href = 'drama3.html';
+  });
+
+document.getElementById('r4').addEventListener('click', function() {
+    window.location.href = 'drama4.html';
+  });
+
+document.getElementById('r5').addEventListener('click', function() {
+    window.location.href = 'drama5.html';
+  });
+
+document.getElementById('r6').addEventListener('click', function() {
+    window.location.href = 'drama6.html';
+  });
+
+document.getElementById('r7').addEventListener('click', function() {
+    window.location.href = 'drama7.html';
+  });
+
+document.getElementById('r8').addEventListener('click', function() {
+    window.location.href = 'drama8.html';
+  });
+
+document.getElementById('r9').addEventListener('click', function() {
+    window.location.href = 'drama9.html';
+  });
+
+document.getElementById('r10').addEventListener('click', function() {
+    window.location.href = 'drama10.html';
+  });
